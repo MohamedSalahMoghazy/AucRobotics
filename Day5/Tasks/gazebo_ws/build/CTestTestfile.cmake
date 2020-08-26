fd50d1d762fe235e7c44f464fc4409cec5ae32e2
Mohamed Salah Moghazy <medosalah977@gmail.com>
2020-08-26 06:02:17 +0200

    Day5 Task Building a mobile robot and a world

---
+++ Day5/Tasks/gazebo_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamed/Documents/AucRobotics/Day5/Tasks/gazebo_ws/src
+# Build directory: /home/mohamed/Documents/AucRobotics/Day5/Tasks/gazebo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_pkg")
